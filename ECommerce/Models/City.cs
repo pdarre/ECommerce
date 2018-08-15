d82cd1fc493db5cb312d635867b5dbc0e0713cd5
pdarre <pdarre.ort@gmail.com>
2018-08-15 17:50:49 -0300

    video5, crud de City, ajuste de dataanotations, relacion uno a muchos(department, city)

--- ECommerce/Models/City.cs
+++ ECommerce/Models/City.cs
@@ -3,17 +3,22 @@
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Web;
 
-    public class Department
+    public class City
     {
         [Key]
-        public int DepartmentId { get; set; }
+        public int CityId { get; set; }
 
         [Required(ErrorMessage = "The field {0} is required")]
         [MaxLength(50, ErrorMessage = "The field {0} must be maximun {1} characters length")]
-        [Display(Name="Department")]
+        [Display(Name="City")]
         public String Name { get; set; }
 
-        public virtual ICollection<City> Cities { get; set; }
+        [Required(ErrorMessage = "The field {0} is required")]
+        public int DepartmentId { get; set; }
+
+        public virtual Department Department { get; set; }
     }
 }
--- ECommerce/Models/ECommerceContext.cs
+++ ECommerce/Models/ECommerceContext.cs
@@ -10,5 +10,7 @@
         }
 
         public System.Data.Entity.DbSet<ECommerce.Models.Department> Departments { get; set; }
+
+        public System.Data.Entity.DbSet<ECommerce.Models.City> Cities { get; set; }
     }
 }
